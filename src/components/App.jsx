945ba54d22debabea61fc5cc864c1a7c75ae417e
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-03-03 22:57:13 -0800

    add photos to docs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
 import './App.scss';
 import Navbar from "./Navbar"
 import Showcase from "./Showcase"
+import { useEffect, useState } from 'react'
+import fetchPhotos from '../data/fetchPhotos';
 
 function App() {
+  const [photo, setPhoto] = useState("");
+
+  useEffect(() => {
+    fetchPhotos()
+    return (res) => {
+      setPhoto(res)
+    }
+  }, [])
+  console.log("photos:", photo)
+
   return (
     <main>
       <header>
         <Navbar />
       </header>
       <section>
-        <Showcase />
+        <Showcase 
+          // photos = {photo}
+        />
       </section>
     </main>
   );
