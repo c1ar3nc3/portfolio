6b6e0112d136ed8a410bb7a6c727df04f6ae21dc
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-03-07 23:56:42 -0800

    add logo and position navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,12 @@
 import './index.scss'
+import logo from "/Users/c1ar3nc3/Projects/portfolio/src/docs/Skeh Nek'huna Hohdidul'eh.png"
 
 export default function Navbar() {
   return (
     <div className="navbar">
+      <div className="main-logo">
+        <img src={logo} alt="logo"/>
+      </div>  
       <div className="title">
         <h3>Skeh Nek'huna Hohdidul'eh</h3>
       </div>
