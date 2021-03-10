fe59b30e5733fd56a45aeef19758bd852346a36f
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-03-09 21:29:32 -0800

    style contact section

--- src/components/Showcase/Contact.jsx
+++ src/components/Showcase/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Contact.scss'
 import { FaEnvelope, FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa"
+import artstarts from '/Users/c1ar3nc3/Projects/portfolio/src/docs/artstarts-logo.png'
 
 export default function Contact() {
   return (
@@ -9,11 +10,18 @@ export default function Contact() {
         <h1>Contact</h1>
       </div>
       <div className="contact-body">
-        <div className="touring">
-          <h3>Touring Artist</h3>
+        <div className="art-logo">
+          <img src={artstarts} alt="artstarts" />
         </div>
-        <div className="art-starts">
-          <h3>Artist in the Classroom</h3>
+        <div className="art-cards">
+          <div className="art-starts">
+            <h3>Touring Artist</h3>
+            <button className="art-button">artstarts Profile</button>
+          </div>
+          <div className="art-starts">
+            <h3>Artist in the Classroom</h3>
+            <button className="art-button">artstarts Profile</button>
+          </div>
         </div>
       </div>
       <div className="contact-footer">
